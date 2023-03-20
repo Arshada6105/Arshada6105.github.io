4290e4d5f5b9c3273ab65198fa63af5f4bb9773e
Arshada6105 <arshada6105@gmail.com>
2023-03-20 20:18:28 +0530

    d

--- port.js
+++ port.js
@@ -1,2 +1,25 @@
 
 
+document.querySelector(".home").addEventListener("click",()=>{
+     document.querySelector("#home").scrollIntoView();
+   
+})
+
+document.querySelector(".about").addEventListener("click",()=>{
+    document.querySelector("#about").scrollIntoView();
+   
+})
+
+document.querySelector(".skills").addEventListener("click",()=>{
+    document.querySelector("#skills").scrollIntoView();
+   
+})
+
+document.querySelector(".projects").addEventListener("click",()=>{
+    document.querySelector("#projects").scrollIntoView();
+   
+})
+document.querySelector(".contact").addEventListener("click",()=>{
+    document.querySelector("#contact").scrollIntoView();
+   
+})
